3ea8ced49fc2c66fca79cdc0fdf36dd17f853b76
fpumir <fpumir@terredeweb.fr>
2015-01-22 10:12:47 +0100

    exec + mapper
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- index.php
+++ index.php
@@ -1,32 +1,18 @@
 <?php
 $f3=require('lib/base.php');
 $f3->set('UI','templates/');
+$f3->set('DEBUG',2);
 
 $f3->route('GET /',function($f3){
   echo View::instance()->render('main.html');
 });
 
 $f3->route('GET /users/@promo',function($f3,$params){
-  $datas=array(
-    'h1'=>array(
-      array('firstname'=>'Jean Claude','lastname'=>'Dus','id'=>1),
-      array('firstname'=>'Benjamin','lastname'=>'Corsini','id'=>2),
-      array('firstname'=>'Robin','lastname'=>'Michet','id'=>3)
-    ),
-    'h2'=>array(
-      array('firstname'=>'Sylvester','lastname'=>'Stallone','id'=>4),
-      array('firstname'=>'Benjamin','lastname'=>'Pumir','id'=>5),
-      array('firstname'=>'Robin','lastname'=>'Rua','id'=>6)
-    ),
-    'h3'=>array(
-      array('firstname'=>'Jean-Christophe','lastname'=>'Beaux','id'=>7),
-      array('firstname'=>'Francois','lastname'=>'Pumir','id'=>8),
-      array('firstname'=>'Super','lastname'=>'Man','id'=>9)
-    )
-  );
-  if($datas[$params['promo']]){
-    $f3->set('datas',$datas[$params['promo']]);
-  }
+  $dB=new DB\SQL('mysql:host=127.0.0.1;port=3306;dbname=wtfay','root','');
+  //$f3->set('data',$dB->exec('SELECT * FROM wifiloc WHERE promo=?',array(1=>$params['promo'])));
+  $users=new DB\SQL\Mapper($dB,'wifiloc');
+  $data=$users->find(array('promo=?',$params['promo']));
+  $f3->set('data',$data);
   echo View::instance()->render('main.html');
 });
 
